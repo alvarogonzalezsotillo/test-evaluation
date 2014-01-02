74303a8a65fd8fea954c9d2a13f5c771b847b060
alvaro <alvarogonzalezsotillo@gmail.com>
2014-01-02 18:47:56 +0100

    Better rendering

--- src/main/scala/evaluation/gui/ImagePanel.scala
+++ src/main/scala/evaluation/gui/ImagePanel.scala
@@ -2,10 +2,10 @@ package evaluation.gui
 
 import javax.swing.JPanel
 import java.awt.{Color, RenderingHints, Graphics2D, Graphics}
+import java.awt.RenderingHints._
 import scala.actors.Actor
 import evaluation.actor.ImagePanelActor
 import evaluation.engine.Img
-import java.awt.image.AffineTransformOp
 import java.awt.geom.AffineTransform
 
 /**
@@ -16,14 +16,14 @@ import java.awt.geom.AffineTransform
  * To change this template use File | Settings | File Templates.
  */
 object ImagePanel {
-  def apply(imageActor: Actor, label: String ) = {
+  def apply(imageActor: Actor, label: String) = {
     val ip = new ImagePanel
     ip.label = label
     val ipa = new ImagePanelActor(imageActor, ip)
     ip
   }
 
-  def apply(image: Img, label: String ) = {
+  def apply(image: Img, label: String) = {
     val ip = new ImagePanel
     ip.label = label
     ip.image = image
@@ -40,14 +40,16 @@ class ImagePanel() extends JPanel {
   private var _label = "Change label"
 
   def image: Img = _image
+
   def image_=(n: Img) {
     _image = n
     repaint()
   }
 
   def label = _label
-  def label_=(l: String){
-    _label =l
+
+  def label_=(l: String) {
+    _label = l
     repaint()
   }
 
@@ -61,9 +63,15 @@ class ImagePanel() extends JPanel {
     def paintImage {
       if (image != null && image.visualizable != null) {
         val g2d = g.create().asInstanceOf[Graphics2D]
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        List(
+          (KEY_ANTIALIASING, VALUE_ANTIALIAS_ON),
+          (KEY_RENDERING, VALUE_RENDER_QUALITY),
+          (KEY_INTERPOLATION, VALUE_INTERPOLATION_BICUBIC)
+        ).foreach(p => g2d.setRenderingHint(p._1, p._2))
+
+
         val img = image.visualizable
-        val af = AffineTransform.getScaleInstance(size.getWidth/img.getWidth, size.getHeight/img.getHeight)
+        val af = AffineTransform.getScaleInstance(size.getWidth / img.getWidth, size.getHeight / img.getHeight)
         g2d.transform(af)
         g2d.drawImage(img, 0, 0, null)
         g2d.dispose()
@@ -72,12 +80,12 @@ class ImagePanel() extends JPanel {
     def paintLabel {
       val margin = 5
       g.setColor(Color.WHITE)
-      (-1 to 1).foreach( dx =>
-        (-1 to 1).foreach( dy => g.drawString(label,margin+dx, size.height - margin + dy)
+      (-1 to 1).foreach(dx =>
+        (-1 to 1).foreach(dy => g.drawString(label, margin + dx, size.height - margin + dy)
         )
       )
       g.setColor(Color.BLACK)
-      g.drawString(label,margin, size.height-margin)
+      g.drawString(label, margin, size.height - margin)
     }
 
     paintImage
